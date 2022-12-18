74bf8059a67266eb02df29295a5c9a51abba50c7
Damian Balon <damian.ba7@gmail.com>
2022-12-18 22:28:33 +0100

    Ostateczna wersja

--- example-app/app/Http/Controllers/Api/PeopleController.php
+++ example-app/app/Http/Controllers/Api/PeopleController.php
@@ -1,40 +1,40 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PeoplePostRequest;
 use App\Models\People;
 use Illuminate\Http\Request;
+use Illuminate\Http\Support\Facades\DB;
+use Illuminate\Http\JsonResponse;
+use App\Http\Resources\PostResource;
+
 
 class PeopleController extends Controller
 {
-   public function index(){
+    public function index(): JsonResponse
+    {
         return response()->json(People::all(), 200);
     }
 
-    public function show($people): JsonResponse{
-        $people = People::find($people);
-        if (is_null($people))
-            return $this->sendError('Person not found');=
-        return response()->json([$people], 200);
-
+    public function show(People $people): JsonResponse
+    {
+        return response()->json($people, 200);
     }
 
-    public function store(Request $request){
-        $people = People::create($request->all());
-        return response()->json([$people], 201);
-
+    public function update(People $people): JsonResponse
+    {
+        return response()->json($people, 201);
     }
 
-    public function update(Request $request, People $people): JsonResponse{
-        $people->update($request->all());
-        return response()->json([$people], 202);
-
+    public function delete(People $people): JsonResponse
+    {
+        return response()->json($people, 204);
     }
 
-    public function delete(People $people)
-        $people->delete();
-        return response()->json([$people], 202);
-
-    } 
+    public function store(People $people): JsonResponse
+    {
+        return response()->json($people, 201);
+    }
 }
--- example-app/routes/api.php
+++ example-app/routes/api.php
@@ -1,25 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\api\PeopleController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
-Route::get('/balon/305343/people', [PeopleControler::class, 'index']);
-Route::get('/balon/305343/people/{people}', [PeopleControler::class, 'show']);
-Route::post('/balon/305343/people', [PeopleControler::class, 'store']);
-Route::put('/balon/305343/people/{people}', [PeopleControler::class, 'update']);
-Route::delete('/balon/305343/people/{people}', [PeopleControler::class, 'delete']);
+Route::get('/balon/305343/people', [PeopleController::class, 'index']);
+Route::get('/balon/305343/people/{people}', [PeopleController::class, 'show']);
+Route::post('/balon/305343/people', [PeopleController::class, 'store']);
+Route::put('/balon/305343/people/{people}', [PeopleController::class, 'update']);
+Route::delete('/balon/305343/people/{people}', [PeopleController::class, 'delete']);
