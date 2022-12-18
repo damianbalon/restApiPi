ffc9d6f447b29900fbfcb31f738662037785875a
Damian Balon <damian.ba7@gmail.com>
2022-12-18 16:34:41 +0100

    Kontroler

---
+++ example-app/app/Http/Controllers/Api/PeopleController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\People;
+use Illuminate\Http\Request;
+
+class PeopleController extends Controller
+{
+   public function index(){
+        return response()->json(People::all(), 200);
+    }
+
+    public function show($people): JsonResponse{
+        $people = People::find($people);
+        if (is_null($people))
+            return $this->sendError('Person not found');=
+        return response()->json([$people], 200);
+
+    }
+
+    public function store(Request $request){
+        $people = People::create($request->all());
+        return response()->json([$people], 200);
+
+    }
+
+    public function update(Request $request, People $people): JsonResponse{
+        $people->update($request->all());
+        return response()->json([$people], 200);
+
+    }
+
+    public function delete(People $people)
+        $people->delete();
+        return response()->json([$people], 200);
+
+    } 
+}
